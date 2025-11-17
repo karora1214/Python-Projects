e842a53b50be5a82dbf6d10d2b6369e2b3e1d19d
Grey <karora1214@gmail.com>
2025-11-17 18:56:32 +0530

    Delete py0001-Band Name Generator.py

--- py0001-Band Name Generator.py
+++
@@ -1,6 +0,0 @@
-# Band Name Generator
-
-print("Welcome to the band name generator")
-city_name = input("Enter the name of the city you were born in : ")
-pet_name = input("Enter the name of your pet : ")
-print("Your band name could be " + city_name + " " + pet_name)
